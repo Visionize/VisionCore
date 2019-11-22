0803c399c3c5b08faf28a0c3b5d6f6e214d8cd7a
Nishkrit Desai <nishkritdesai10@gmail.com>
2019-11-21 19:48:18 -0500

    Bash script works even when arguments are switched

--- run.sh
+++ run.sh
@@ -7,11 +7,14 @@
 #
 
 #Build the latest docker image
-if [ "$1" = "build" ]; then
+if [[ "$@" == *"build"* ]]; then
+    echo "Building Container"
     sudo docker build -t visioncore .
+    echo "Finished building container"
 fi
 
 #Start the docker container
-if [ "$2" = "run" ]; then 
+if [[ "$@" == *"run"* ]]; then 
+    echo "Starting container"
     sudo docker run -p 8888:8888 --gpus all -v ~/Code/Visionize/VisionCore/fruit_photos:/VisionCore/fruit_photos visioncore
 fi
